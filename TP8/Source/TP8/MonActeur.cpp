6a362f012fd22dbb581cbf9f674fe25dadfc678e
Mathieu <tsunoll96@gmail.com>
2020-11-23 15:16:03 +0100

    Part II

---
+++ TP8/Source/TP8/MonActeur.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MonActeur.h"
+
+// Sets default values
+AMonActeur::AMonActeur()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMonActeur::BeginPlay()
+{
+	UE_LOG(LogActor, Warning, TEXT("∗∗∗∗∗∗ HELLO World FROM GAMAGORA  ! ! ! ! "));
+	UE_LOG(LogActor, Warning, TEXT("Actor␣name␣%s "), *this->GetName());
+	int out = 10;
+	UE_LOG(LogActor, Error, TEXT("Test affichage :  %d"), out);
+
+	FString nom = FString::Printf(TEXT("Nom de l'acteur dans une variable %s "), *(this->GetName()));
+	UE_LOG(LogActor, Warning, TEXT("%s"), *nom);
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AMonActeur::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ TP8/Source/TP8/TP8.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TP8.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP8, "TP8" );
